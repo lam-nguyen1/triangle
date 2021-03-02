d60c826efba694199b906a35cf47beffb78c6c65
lam-nguyen1 <nguyen_realmadrid@hotmail.com>
2021-03-02 01:39:18 +0100

    feat: form validation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { useFormik } from 'formik';
 import '@tradeshift/tradeshift-ui';
 import '@tradeshift/tradeshift-ui/ts.css';
 
@@ -5,71 +8,57 @@ import '../styles/styles.less';
 import Polygon from './dom/polygon';
 import { element, removeNode } from './dom/dom';
 
-
-const sideOne = document.querySelector('#side-one');
-const sideTwo = document.querySelector('#side-two');
-const sideThree = document.querySelector('#side-three');
-
-const em = document.querySelector('em');
-
-const getCoords = polygonPoints => {
-  return polygonPoints.split(' ').map(v => {
-    return {
-      x: +v.split(',')[0],
-      y: +v.split(',')[1],
-    };
-  });
+const validateTriLen = (errors, values, val) => {
+  if (!values[val]) {
+    errors[val] = 'Required';
+  } else if (values[val] > 1 || values[val] < 0) {
+    errors[val] = 'Must be between 0 and 1(inclusive). Example: 0.5';
+  }
 }
 
-const getDist = (p1, p2) => {
-  return Math.sqrt((p2.x - p1.x) ** 2 + (p2.y - p1.y) ** 2);
-}
+const validate = values => {
+  const errors = {};
+  validateTriLen(errors, values, 'sideOne');
+  validateTriLen(errors, values, 'sideTwo');
+  validateTriLen(errors, values, 'sideThree');
 
-const isTriangle = (s1, s2, s3) => {
-  return s1 + s2 > s3 && s1 + s3 > s2 && s2 + s3 > s1;
-}
+  return errors;
+};
 
-const addPolygon = (points) => {
-  return Polygon(
-    points.x, 
-    points.y,
-    points.x1,
-    points.y1,
-    points.x2,
-    points.y2,
-  );
-}
-
-ts.ui.ready(() => {
-  const initPolygon = addPolygon(
-    {
-      x: 250,
-      y: 378940/2703,
-      x1: 100,
-      y1: 400,
-      x2: 400,
-      y2: 400,
+const TriangleSidesForm = () => {
+  const formik = useFormik({
+    initialValues: {
+      sideOne: '',
+      sideTwo: '',
+      sideThree: '',
     },
-  );
-  const svg1 = element('svg', {width: '500', height: '500'}, [initPolygon])
-  let triCont = element('div', { class: 'triangle-container' }, [svg1]);
-  em.appendChild(triCont);
+    validate,
+    onSubmit: values => {
+      const initPolygon = addPolygon(
+        {
+          x: 250,
+          y: 378940/2703,
+          x1: 100,
+          y1: 400,
+          x2: 400,
+          y2: 400,
+        },
+      );
+      const svg1 = element('svg', {width: '500', height: '500'}, [initPolygon])
+      let triCont = element('div', { class: 'triangle-container' }, [svg1]);
+      em.appendChild(triCont);
+    
+      const coords = getCoords(initPolygon.getAttribute('points'));
+      const dist1 = getDist(coords[0], coords[1]);
+      const dist2 = getDist(coords[0], coords[2]);
+      const dist3 = getDist(coords[1], coords[2]);
+      let polygon;
 
-  const coords = getCoords(initPolygon.getAttribute('points'));
-  const dist1 = getDist(coords[0], coords[1]);
-  const dist2 = getDist(coords[0], coords[2]);
-  const dist3 = getDist(coords[1], coords[2]);
-  let polygon;
+      const { sideOne, sideTwo, sideThree } = values;
 
-  ts.ui.Header.buttons([
-    {label: 'Set sides', type: 'ts-primary', onclick: () => {
-      const s1 = +sideOne.value;
-      const s2 = +sideTwo.value;
-      const s3 = +sideThree.value;
-
-      const alpha = s1 * dist1;
-      const beta = s2 * dist2;
-      const gamma = s3 * dist3;
+      const alpha = sideOne * dist1;
+      const beta = sideTwo * dist2;
+      const gamma = sideThree * dist3;
 
       const isTri = isTriangle(alpha, beta, gamma);
 
@@ -100,8 +89,127 @@ ts.ui.ready(() => {
         em.appendChild(triCont);
 
       } else {
+        removeNode(em, triCont);
         alert('No new triangle rendered! Please make sure that the triangle can be constructed by making sure that the sum of two arbitrary sides is greater than the last side.');
       }
-    }}
-  ]);
-});
+    },
+  });
+
+  return (
+    <div data-ts="Main">
+      <div data-ts="Content">
+        <div data-ts="Panel">
+          <em>
+            <form data-ts="Form" onSubmit={formik.handleSubmit}>
+              <fieldset>
+                <label htmlFor="sideOne">
+                  <span>Enter length of triangle side 1</span>
+                  <input 
+                    id="sideOne"
+                    name="sideOne"
+                    type="number"
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    value={formik.values.sideOne}
+                  />
+                </label>
+                {
+                  formik.touched.sideOne && formik.errors.sideOne ? 
+                    <dl className="ts-errors">
+                      <dt>{ formik.errors.sideOne }</dt>
+                    </dl> :
+                    null
+                }
+              </fieldset>
+
+              <fieldset>
+                <label htmlFor="sideTwo">
+                  <span>Enter length of triangle side 2</span>
+                  <input 
+                    id="sideTwo"
+                    name="sideTwo"
+                    type="number"
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    value={formik.values.sideTwo}
+                  />
+                </label>
+                {
+                   formik.touched.sideTwo && formik.errors.sideTwo ? 
+                    <dl className="ts-errors">
+                      <dt>{ formik.errors.sideTwo }</dt>
+                    </dl> :
+                    null
+                }
+              </fieldset>
+
+              <fieldset>
+                <label htmlFor="sideThree">
+                  <span>Enter length of triangle side 3</span>
+                  <input 
+                    id="sideThree"
+                    name="sideThree"
+                    type="number"
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    value={formik.values.sideThree}
+                  />
+                </label>
+                {
+                  formik.touched.sideThree && formik.errors.sideThree ? 
+                    <dl className="ts-errors">
+                      <dt>{ formik.errors.sideThree }</dt>
+                    </dl> :
+                    null
+                }
+              </fieldset>
+
+              <fieldset>
+                <button data-ts="Button" className="ts-primary" type="submit">
+                  <span>Set sides</span>
+                </button>
+              </fieldset>
+            </form>
+          </em>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+const App = () => {
+  return <TriangleSidesForm />
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
+
+const em = document.querySelector('em');
+
+const getCoords = polygonPoints => {
+  return polygonPoints.split(' ').map(v => {
+    return {
+      x: +v.split(',')[0],
+      y: +v.split(',')[1],
+    };
+  });
+}
+
+const getDist = (p1, p2) => {
+  return Math.sqrt((p2.x - p1.x) ** 2 + (p2.y - p1.y) ** 2);
+}
+
+const isTriangle = (s1, s2, s3) => {
+  return s1 + s2 > s3 && s1 + s3 > s2 && s2 + s3 > s1;
+}
+
+const addPolygon = (points) => {
+  return Polygon(
+    points.x, 
+    points.y,
+    points.x1,
+    points.y1,
+    points.x2,
+    points.y2,
+  );
+}
