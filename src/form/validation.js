48f886e820d4bbefca64f96558369e9d25801352
lam-nguyen1 <nguyen_realmadrid@hotmail.com>
2021-03-03 19:35:26 +0100

    fix(form): check nan on typeof

--- src/form/validation.js
+++ src/form/validation.js
@@ -1,7 +1,7 @@
 const validateTriLen = (errors, values, val) => {
   if (values[val].length === 0) {
     errors[val] = 'This field is required. Please input a number.';
-  } else if (Number.isNaN(values[val])) {
+  } else if (typeof values[val] !== 'number') {
     errors[val] = 'Must be a number';
   } else if (values[val] <= 0) {
     errors[val] = 'Length must be larger than zero. Example: 3';
