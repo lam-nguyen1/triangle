d736fd718f3d2c9405cace2c194a120f897de8d4
lam-nguyen1 <nguyen_realmadrid@hotmail.com>
2021-03-03 18:23:05 +0100

    test: interaction

---
+++ src/__tests__/components/triangleSidesForm.test.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import { TriangleSidesForm } from '../../components/triangleSidesForm';
+
+test('setting valid triangle sides and rendering triangle', async () => {
+  const handleRender = jest.fn();
+
+  const { container } = render(<TriangleSidesForm onRender={handleRender} />);
+
+  userEvent.type(screen.getByLabelText(/Enter length of triangle side 1/i), '3');
+  userEvent.type(screen.getByLabelText(/Enter length of triangle side 2/i), '4');
+  userEvent.type(screen.getByLabelText(/Enter length of triangle side 3/i), '5');
+
+  fireEvent.click(screen.getByText('Set sides'));
+
+  await waitFor(() => {
+    expect(container.querySelector('svg')).toBeTruthy();
+    expect(handleRender).toHaveBeenCalledWith({
+      sideOne: 3,
+      sideTwo: 4,
+      sideThree: 5,
+    });
+  });
+});
+
+test('should not render invalid triangle', async () => {
+  jest.spyOn(window, 'alert').mockImplementation(() => {});
+  const handleRender = jest.fn();
+
+  const { container } = render(<TriangleSidesForm onRender={handleRender} />);
+
+  userEvent.type(screen.getByLabelText(/Enter length of triangle side 1/i), '3');
+  userEvent.type(screen.getByLabelText(/Enter length of triangle side 2/i), '4');
+  userEvent.type(screen.getByLabelText(/Enter length of triangle side 3/i), '8');
+
+  fireEvent.click(screen.getByText('Set sides'));
+
+  await waitFor(() => {
+    expect(container.querySelector('svg')).toBeFalsy();
+    expect(handleRender).toHaveBeenCalledWith({
+      sideOne: 3,
+      sideTwo: 4,
+      sideThree: 8,
+    });
+  });
+});
